7e4208210773d3e35f13f32852cba8b8c0306c3d
charles <xquireworld@gmail.com>
2017-01-11 04:34:19 +0000

    enter code

--- find missing/app/library.js
+++ find missing/app/library.js
@@ -1,3 +1,30 @@
-module.exports = {
+module.exports={
 
-}
+findMissing: function(arr1,arr2){
+
+  var firstArrayLength=arr1.length;
+  
+  var secondArrayLength=arr2.length;
+  
+  var result=[];
+  
+  for (var i=0; i<firstArrayLength; i++){
+    if (arr2.indexOf(arr1[i]) === -1){
+      result.push(arr1[i]);
+    }
+  }
+  
+  for (i=0; i<secondArrayLength; i++) {
+    if (arr1.indexOf(arr2[i]) === -1) {
+      result.push(arr2[i]);
+    }
+  }
+
+  if(firstArrayLength===secondArrayLength){
+    return 0;
+  }else{
+    return result;
+    }
+  }
+
+ }
